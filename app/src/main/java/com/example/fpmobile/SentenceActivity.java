03292fe9bdec71edefe3fcbcc5dc60f0faee5a8f
christopheradriankusuma <christopheradrian2903@gmail.com>
2021-12-24 09:10:06 +0700

    Progress sentence page

--- app/src/main/java/com/example/fpmobile/SentenceActivity.java
+++ app/src/main/java/com/example/fpmobile/SentenceActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class SentenceActivity extends AppCompatActivity {
 
@@ -17,14 +18,14 @@ public class SentenceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sentence);
 
-        ArrayList<String> arrayList = new ArrayList<>();
-        arrayList.add("Hello 1");
-        arrayList.add("Hello 2");
-        arrayList.add("Hello 3");
-        arrayList.add("Hello 4");
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.row, arrayList);
         ListView listView = findViewById(R.id.vertical_list);
-        listView.setAdapter(arrayAdapter);
+
+        ArrayList<ArrayList<Item>> arrayList = new ArrayList<>();
+
+        ArrayList<Item> row1 = new ArrayList<Item>();
+
+        ItemAdapter itemAdapter = new ItemAdapter(getApplicationContext(), arrayList);
+        listView.setAdapter(itemAdapter);
 
         Button backButton = findViewById(R.id.arrowBack);
         backButton.setOnClickListener(new View.OnClickListener() {
